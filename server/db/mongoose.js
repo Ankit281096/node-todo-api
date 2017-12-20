2011a06bffac2082a41acf5b0c0f8c86bef8117f
Ankit <ankit.son.og@gmail.com>
2017-12-20 08:45:10 +0530

    some changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-let db={
-  localhost:'mongodb://localhost:27017/TodoApp',
-  mlab:'mongodb://<Ankit>:<Ankit>@ds115035.mlab.com:15035/todoappdeployment'
-};
-mongoose.connect( process.env.MONGODB_URI ? db.mlab : db.localhost);
+// let db={
+//   localhost:'mongodb://localhost:27017/TodoApp',
+//   mlab:'mongodb://<Ankit>:<Ankit>@ds115035.mlab.com:15035/todoappdeployment'
+// };
+MONGODB_URI='mongodb://<Ankit>:<Ankit>@ds115035.mlab.com:15035/todoappdeployment'
+mongoose.connect( MONGODB_URI  || 'mongodb://localhost:27017/TodoApp' );
 
 module.exports={mongoose};
