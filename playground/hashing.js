c9cd1a88519c466adf7dc7283df3ddca29bd9f17
Ankit <ankit.son.og@gmail.com>
2017-12-22 09:22:02 +0530

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,25 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt =require('bcryptjs');
 
-var data ={
-  id:10
-};
 
-var token=jwt.sign(data,'123abc');
-console.log(token);
+var password='it@123';
+
+// bcrypt.genSalt(10,(err,salt)=>{
+//   bcrypt.hash(password,salt,(err,hash)=>{
+//     console.log(hash);
+//   })
+// });
+
+bcrypt.compare(password,'$2a$10$iXnyQfbZSY4ay7pyJNfEPOsi/bwtWQSqbLXvi2mdJGvKEuSnFNnEe',(err,res)=>{
+  console.log(res);
+});
+// var data ={
+//   id:10
+// };
+//
+// var token=jwt.sign(data,'123abc');
+// console.log(token);
 // var message ='I am user number 3';
 // var hash = SHA256(message).toString();
 // // var hhash= SHA256(message).toHexString();
